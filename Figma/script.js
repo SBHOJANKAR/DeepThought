e34589430b5cd08c9d4a798fd385a23937448d5d
SBHOJANKAR <92084815+SBHOJANKAR@users.noreply.github.com>
2022-08-11 18:28:00 +0530

    Add files via upload

---
+++ Figma/script.js
@@ -0,0 +1,27 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "312px";
+    document.getElementById("main").style.marginLeft = "312px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+
+// function openNav()
+// {
+//     document.getElementById('bar').style.marginLeft = "312px";
+//     document.getElementById('navbar').style.width = "312px";
+// }
+
+
+
+// if(bar)
+// {
+//     bar.addEventListener('click',() => {
+//         nav.classList.add('active');
+//     })
+// } 
